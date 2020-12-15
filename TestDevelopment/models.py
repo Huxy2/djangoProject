c6e701c398c2216e40598f846ce6155990b5c55b
Administrator <1281255967@qq.com>
2020-12-15 16:58:56 +0800

    ORM框架

--- TestDevelopment/models.py
+++ TestDevelopment/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+
+class People(models.Model):
+    """
+    数据库模型类
+    """
+    name = models.CharField(max_length=30)
+    age = models.IntegerField(default=1)
+    # gender = models.CharField(max_length=2)
+    score = models.IntegerField()
+
+    class Meta:
+        db_table = 'tb_people'
+        verbose_name = ''
+
+
+class TestDevelopment(models.Model):
+    name = models.CharField(max_length=200, verbose_name='项目名称', help_text='项目名称', unique=True)
+    leader = models.CharField(max_length=50, verbose_name='项目负责人', help_text='项目负责人')
+    desc = models.TextField(verbose_name='项目描述', help_text='项目描述', blank=True, null=True, default='')
+
+    class Meta:
+        db_table = 'tb_testdevelopment'
--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'my_django',
+        'USER': 'root',
+        'PASSWORD': 'root123',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
     }
 }
 
